8870630ccdd0d0b4cc974c228b1df37d899ba22a
Samuel Abolade <blessingabolade15@gmail.com>
2023-10-26 14:14:23 +0100

    Added authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,59 +1,56 @@
 const jwt = require('jsonwebtoken');
-const AppError = require('../utils/AppError');
-const catchAsync = require('../utils/catchAsync');
 const User = require('../models/User');
 
-exports.protect = catchAsync(async (req, res, next) => {
-  if (
-    !req.headers.authorization ||
-    !req.headers.authorization.split(' ')[1][10]
-    /* [10] used since JWT are always more than 10 characters in length */
-  )
-    return next(new AppError('You are not logged in, Kindly log in', 401));
+const catchAsync = require('../utils/catchAsync');
+const checkRequiredFields = require('../utils/helpers/checkRequiredFields');
+const AppError = require('../utils/AppError');
 
-  const token = req.headers.authorization.split(' ')[1];
-  const decodedToken = jwt.verify(token, process.env.JWT_SECRETE);
-  console.log(decodedToken);
+function signToken(id) {
+  return jwt.sign({ id }, process.env.JWT_SECRETE, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+}
 
-  // // 3) Check if user still exists
-  const currentUser = await User.findById(decodedToken.id);
-  // if (!currentUser) {
-  //   return next(
-  //     new AppError(
-  //       'The user belonging to this token does no longer exist.',
-  //       401,
-  //     ),
-  //   );
-  // }
+exports.register = catchAsync(async (req, res, next) => {
+  checkRequiredFields(req.body, ['firstname', 'lastname', 'email', 'password']);
+  req.body.role = undefined;
+  const doc = new User(req.body);
+  await doc.matchPasswords(req.body.passwordConfirm);
+  await doc.save();
 
-  // // 4) Check if user changed password after the token was issued
-  // if (currentUser.changedPasswordAfter(decodedToken.iat)) {
-  //   return next(
-  //     new AppError('User recently changed password! Please log in again.', 401),
-  //   );
-  // }
+  doc.password = undefined;
+  doc.__v = undefined;
+  // const token = signToken(doc._id);
 
-  // GRANT ACCESS TO PROTECTED ROUTE
-  req.user = currentUser;
-  res.locals.user = currentUser;
-  next();
+  res.status(201).json({
+    message: 'Registration Successful',
+    // token,
+    doc,
+  });
 });
 
-exports.restrictTo =
-  (...roles) =>
-  (req, res, next) => {
-    // roles ['admin', 'applicant']. role='user'
-    // if (req.user.role ==='applicant'){
-    //   const student = await Applicant.findById(req.user.id)
-    //   if (!student) return "This route is restricted to only Applicants"
-    // }
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+  checkRequiredFields(req.body, ['email', 'password']);
 
-    if (!roles.includes(req.user.role)) {
-      return next(
-        new AppError('You do not have permission to perform this action', 403),
-      );
-    }
+  const doc = await User.findOne({ email }).select('+password');
 
-    // if (!roles.include)
-    next();
-  };
+  if (!doc || !(await doc.verifyPassword(password)))
+    return next(new AppError('Invalid email or password', 400));
+  const token = signToken(doc._id);
+
+  //   res.cookie('jwt', token, {
+  //     expires: new Date(
+  //       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
+  //     ),
+  //     // httpOnly: true,
+  //     // secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
+  //   });
+
+  res.status(200).json({
+    status: 'success',
+    token: token,
+  });
+});
+
+exports.protect = catchAsync;
